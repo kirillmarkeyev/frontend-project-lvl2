b1013bb5fe9e519d0e0d17df2a6b10d0b1c79527
kirillmarkeyev <kirillmarkeyev@mail.ru>
2022-02-05 01:56:45 +0300

    Change const absoluteFilePath

--- src/temp.js
+++ src/temp.js
@@ -2,13 +2,11 @@ import fs from 'fs';
 // import { cwd } from 'process';
 import path from 'path';
 
-
 const getFileData = (filepath) => {
-    const absoluteFilePath = path.resolve(filepath);
+    const absoluteFilePath = path.isAbsolute(filepath) ? filepath : path.resolve(filepath);
     const file = fs.readFileSync(absoluteFilePath, 'utf-8');
     return JSON.parse(file);
-}
-
+};
 
 const someTempFunction = (filepath1, filepath2) => {
     const data1 = getFileData(filepath1);
