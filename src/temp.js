56fa5b54e5f50914bdcaee31875ed6c6bb9cbdfe
kirillmarkeyev <kirillmarkeyev@mail.ru>
2022-02-04 23:56:11 +0300

    Add two temp functions to temp.js

---
+++ src/temp.js
@@ -0,0 +1,19 @@
+import fs from 'fs';
+// import { cwd } from 'process';
+import path from 'path';
+
+
+const getFileData = (filepath) => {
+    const absoluteFilePath = path.resolve(filepath);
+    const file = fs.readFileSync(absoluteFilePath, 'utf-8');
+    return JSON.parse(file);
+}
+
+
+const someTempFunction = (filepath1, filepath2) => {
+    const data1 = getFileData(filepath1);
+    const data2 = getFileData(filepath2);
+    console.log(data1);
+    console.log(data2);
+};
+export default someTempFunction;
