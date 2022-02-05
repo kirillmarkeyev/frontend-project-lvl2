8338c3dbf2c0e4f0ac2014a77785500d8f8655cc
kirillmarkeyev <kirillmarkeyev@mail.ru>
2022-02-05 20:06:22 +0300

    Replace cycle "for ... of" with reduce()

--- src/temp.js
+++ src/temp.js
@@ -1,5 +1,3 @@
-/* eslint no-restricted-syntax: ["off", "ForOfStatement"] */
-
 import fs from 'fs';
 import process from 'process';
 import path from 'path';
@@ -19,19 +17,20 @@ const someTempFunction = (filepath1, filepath2) => {
   const keys1 = _.keys(data1);
   const keys2 = _.keys(data2);
   const keys = _.sortBy(_.union(keys1, keys2));
-  const result = [];
-  for (const key of keys) {
+
+  const result = keys.reduce((acc, key) => {
     if (!_.has(data1, key)) {
-      result.push(`  + ${key}: ${data2[key]}`);
+      acc.push(`  + ${key}: ${data2[key]}`);
     } else if (!_.has(data2, key)) {
-      result.push(`  - ${key}: ${data1[key]}`);
+      acc.push(`  - ${key}: ${data1[key]}`);
     } else if (data1[key] !== data2[key]) {
-      result.push(`  - ${key}: ${data1[key]}`);
-      result.push(`  + ${key}: ${data2[key]}`);
+      acc.push(`  - ${key}: ${data1[key]}`);
+      acc.push(`  + ${key}: ${data2[key]}`);
     } else {
-      result.push(`    ${key}: ${data1[key]}`);
+      acc.push(`    ${key}: ${data1[key]}`);
     }
-  }
+    return acc;
+  }, []);
 
   return `{\n${result.join('\n')}\n}`;
 };
