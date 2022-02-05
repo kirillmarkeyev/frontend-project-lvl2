a273a1d33fa6497b951414675daf77117deb618e
kirillmarkeyev <kirillmarkeyev@mail.ru>
2022-02-05 18:55:40 +0300

    Realize diff function for step #3 of project

--- src/temp.js
+++ src/temp.js
@@ -1,9 +1,13 @@
+/* eslint no-restricted-syntax: ["off", "ForOfStatement"] */
+
 import fs from 'fs';
-// import { cwd } from 'process';
+import process from 'process';
 import path from 'path';
+import _ from 'lodash';
 
 const getFileData = (filepath) => {
-  const absoluteFilePath = path.isAbsolute(filepath) ? filepath : path.resolve(filepath);
+  const absoluteFilePath = path.isAbsolute(filepath)
+    ? filepath : path.resolve(process.cwd(), filepath);
   const file = fs.readFileSync(absoluteFilePath, 'utf-8');
   return JSON.parse(file);
 };
@@ -11,7 +15,24 @@ const getFileData = (filepath) => {
 const someTempFunction = (filepath1, filepath2) => {
   const data1 = getFileData(filepath1);
   const data2 = getFileData(filepath2);
-  console.log(data1);
-  console.log(data2);
+
+  const keys1 = _.keys(data1);
+  const keys2 = _.keys(data2);
+  const keys = _.sortBy(_.union(keys1, keys2));
+  const result = [];
+  for (const key of keys) {
+    if (!_.has(data1, key)) {
+      result.push(`  + ${key}: ${data2[key]}`);
+    } else if (!_.has(data2, key)) {
+      result.push(`  - ${key}: ${data1[key]}`);
+    } else if (data1[key] !== data2[key]) {
+      result.push(`  - ${key}: ${data1[key]}`);
+      result.push(`  + ${key}: ${data2[key]}`);
+    } else {
+      result.push(`    ${key}: ${data1[key]}`);
+    }
+  }
+
+  return `{\n${result.join('\n')}\n}`;
 };
 export default someTempFunction;
