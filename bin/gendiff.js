12ab26c9a6f10f1ddfa81bb103aa03d4cd921131
kirillmarkeyev <kirillmarkeyev@mail.ru>
2022-02-05 03:18:36 +0300

    Change program.parse()

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -8,11 +8,10 @@ const program = new Command();
 program
     .description('Compares two configuration files and shows a difference.')
     .version('0.0.1', '-V, --version', 'output the version number')
-    .argument('<filepath1>')
-    .argument('<filepath2>')
+    .arguments('<filepath1> <filepath2>')
     .option('-f, --format <type>', 'output format')
     .action((filepath1, filepath2) => {
         someTempFunction(filepath1, filepath2);
     });
 
-program.parse();
+program.parse(process.argv);
