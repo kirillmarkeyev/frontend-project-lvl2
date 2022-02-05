8dace9a33f3b04349946016682ab3b360be9d5b9
kirillmarkeyev <kirillmarkeyev@mail.ru>
2022-02-05 17:04:27 +0300

    Eslint fix

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -6,12 +6,12 @@ import someTempFunction from '../src/temp.js';
 const program = new Command();
 
 program
-    .description('Compares two configuration files and shows a difference.')
-    .version('0.0.1', '-V, --version', 'output the version number')
-    .arguments('<filepath1> <filepath2>')
-    .option('-f, --format <type>', 'output format')
-    .action((filepath1, filepath2) => {
-        someTempFunction(filepath1, filepath2);
-    });
+  .description('Compares two configuration files and shows a difference.')
+  .version('0.0.1', '-V, --version', 'output the version number')
+  .arguments('<filepath1> <filepath2>')
+  .option('-f, --format <type>', 'output format')
+  .action((filepath1, filepath2) => {
+    someTempFunction(filepath1, filepath2);
+  });
 
 program.parse(process.argv);
--- src/temp.js
+++ src/temp.js
@@ -3,15 +3,15 @@ import fs from 'fs';
 import path from 'path';
 
 const getFileData = (filepath) => {
-    const absoluteFilePath = path.isAbsolute(filepath) ? filepath : path.resolve(filepath);
-    const file = fs.readFileSync(absoluteFilePath, 'utf-8');
-    return JSON.parse(file);
+  const absoluteFilePath = path.isAbsolute(filepath) ? filepath : path.resolve(filepath);
+  const file = fs.readFileSync(absoluteFilePath, 'utf-8');
+  return JSON.parse(file);
 };
 
 const someTempFunction = (filepath1, filepath2) => {
-    const data1 = getFileData(filepath1);
-    const data2 = getFileData(filepath2);
-    console.log(data1);
-    console.log(data2);
+  const data1 = getFileData(filepath1);
+  const data2 = getFileData(filepath2);
+  console.log(data1);
+  console.log(data2);
 };
 export default someTempFunction;
