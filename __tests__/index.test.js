4546d682df0c1ab4a6510c074ba7d28ff41c2b44
kirillmarkeyev <kirillmarkeyev@mail.ru>
2022-02-07 16:35:33 +0300

    Step 5: add test for plain yaml file

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -10,12 +10,20 @@ const __dirname = path.dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
-const expectedResult = readFile('expectedPlainJSON.txt');
-
 test('check diff for two plain json files', () => {
   const filepath1 = getFixturePath('file1.json');
   const filepath2 = getFixturePath('file2.json');
   const receivedResult = genDiff(filepath1, filepath2);
+  const expectedResult = readFile('expectedPlainJSON.txt');
+  expect(receivedResult).toEqual(expectedResult);
+  expect(typeof receivedResult).toBe('string');
+});
+
+test('check diff for two plain yaml files', () => {
+  const filepath1 = getFixturePath('file1.yml');
+  const filepath2 = getFixturePath('file2.yaml');
+  const receivedResult = genDiff(filepath1, filepath2);
+  const expectedResult = readFile('expectedPlainYAML.txt');
   expect(receivedResult).toEqual(expectedResult);
   expect(typeof receivedResult).toBe('string');
 });
