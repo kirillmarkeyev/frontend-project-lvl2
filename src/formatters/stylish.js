8ebd24c4802e0930c5392df6bc670e700e0ba8d8
kirillmarkeyev <kirillmarkeyev@mail.ru>
2022-02-10 01:56:05 +0300

    Step 6: working on function for nested files

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,5 +1,6 @@
-const stylish = (arr) => {
-  const indent = '  ';
+const stringify = (arr, depth = 1) => {
+  const indent = '  '.repeat(depth);
+  const bracket = `${indent}  `;
   const result = arr.map((element) => {
     switch (element.type) {
       case 'removed':
@@ -10,14 +11,15 @@ const stylish = (arr) => {
         return `${indent}- ${element.key}: ${element.removedValue}\n${indent}+ ${element.key}: ${element.addedValue}`;
       case 'unchanged':
         return `${indent}  ${element.key}: ${element.unchangedValue}`;
+      case 'nested':
+        return `${indent}  ${element.key}: {\n${stringify(element.children, depth + 2)}\n${bracket}}`;
       default:
-        throw new Error(`Unknown ${element.type}`);
+        throw new Error(`Unknown type: ${element.type}`);
     }
   });
-  return [
-    '{',
-    ...result,
-    '}',
-  ].join('\n');
+  return result.join('\n');
 };
+
+const stylish = (diffTree) => `{\n${stringify(diffTree)}\n}`;
+
 export default stylish;
