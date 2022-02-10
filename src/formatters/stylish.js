751669c7e12ae6f80e81093e7abf46b3a235b40f
kirillmarkeyev <kirillmarkeyev@mail.ru>
2022-02-10 20:39:45 +0300

    Step 6: rewrite stylish.js and fix working with nested objects

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,25 +1,49 @@
-const stringify = (arr, depth = 1) => {
-  const indent = '  '.repeat(depth);
-  const bracket = `${indent}  `;
-  const result = arr.map((element) => {
-    switch (element.type) {
-      case 'removed':
-        return `${indent}- ${element.key}: ${element.removedValue}`;
-      case 'added':
-        return `${indent}+ ${element.key}: ${element.addedValue}`;
-      case 'changed':
-        return `${indent}- ${element.key}: ${element.removedValue}\n${indent}+ ${element.key}: ${element.addedValue}`;
-      case 'unchanged':
-        return `${indent}  ${element.key}: ${element.unchangedValue}`;
-      case 'nested':
-        return `${indent}  ${element.key}: {\n${stringify(element.children, depth + 2)}\n${bracket}}`;
-      default:
-        throw new Error(`Unknown type: ${element.type}`);
-    }
+import _ from 'lodash';
+
+const spacesCount = 4;
+const getIndent = (depth) => ' '.repeat(depth * spacesCount - 2);
+
+// https://ru.hexlet.io/challenges/js_trees_stringify_exercise
+
+const stringify = (value, depth) => {
+  if (!_.isPlainObject(value)) {
+    return `${value}`;
+  }
+  const keys = _.keys(value);
+  const result = keys.map((key) => {
+    const childValue = value[key];
+    return `${getIndent(depth)}  ${key}: ${stringify(childValue, depth + 1)}\n`;
   });
-  return result.join('\n');
+  return `{\n${result.join('')}${getIndent(depth - 1)}  }`;
 };
 
-const stylish = (diffTree) => `{\n${stringify(diffTree)}\n}`;
+const getDiffTree = (nodes) => {
+  const iter = (node, depth = 1) => {
+    const {
+      key, type, children, removedValue, addedValue, unchangedValue,
+    } = node;
+
+    switch (type) {
+      case 'nested':
+        return `\n${getIndent(depth)}  ${key}: {${children.map((child) => iter(child, depth + 1)).join('')}\n${getIndent(depth)}  }`;
+      case 'unchanged':
+        return `\n${getIndent(depth)}  ${key}: ${stringify(unchangedValue, depth + 1)}`;
+      case 'changed':
+        return `\n${getIndent(depth)}- ${key}: ${stringify(removedValue, depth + 1)}\n${getIndent(depth)}+ ${key}: ${stringify(addedValue, depth + 1)}`;
+      case 'added':
+        return `\n${getIndent(depth)}+ ${key}: ${stringify(addedValue, depth + 1)}`;
+      case 'removed':
+        return `\n${getIndent(depth)}- ${key}: ${stringify(removedValue, depth + 1)}`;
+      default:
+        throw new Error(`Sorry! Type: ${type} is unknown.`);
+    }
+  };
+  return iter(nodes);
+};
+
+const stylish = (diffTree) => {
+  const lines = diffTree.map((node) => getDiffTree(node));
+  return `{${lines.join('')}\n}`;
+};
 
 export default stylish;
