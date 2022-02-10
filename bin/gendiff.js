020307c0fdd8c89f793d5d4c11f79a010ebadeb2
kirillmarkeyev <kirillmarkeyev@mail.ru>
2022-02-10 21:32:35 +0300

    Step 6: set stylish as default option

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -7,11 +7,11 @@ const program = new Command();
 
 program
   .description('Compares two configuration files and shows a difference.')
-  .version('0.0.1', '-V, --version', 'output the version number')
+  .version('0.0.1', '-v, --version', 'output the version number')
   .arguments('<filepath1> <filepath2>')
-  .option('-f, --format <type>', 'output format')
+  .option('-f, --format <type>', 'output format', 'stylish')
   .action((filepath1, filepath2) => {
     console.log(genDiff(filepath1, filepath2));
   });
 
-program.parse();
+program.parse(process.argv);
