cb7027b6958f0a01bb4080a2eba5d70b334274c2
kirillmarkeyev <kirillmarkeyev@mail.ru>
2022-02-15 17:23:24 +0300

    Fix diffTree.js

--- src/diffTree.js
+++ src/diffTree.js
@@ -3,7 +3,8 @@ import _ from 'lodash';
 const getDiffTree = (data1, data2) => {
   const keys1 = _.keys(data1);
   const keys2 = _.keys(data2);
-  const keys = _.sortBy(_.union(keys1, keys2));
+  const unionKeys = _.union(keys1, keys2);
+  const keys = _.sortBy(unionKeys);
 
   const diffTree = keys.map((key) => {
     const value1 = data1[key];
